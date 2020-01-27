21178e40a476bda5070f11d1fb736b802c4ccabf
haya14busa <haya14busa@gmail.com>
2020-01-27 05:27:42 +0000

    Extract only semver part for latest release
    This change excludes needless prefix (e.g. "redpen-" of "redpen-1.10.4"
    tag) for latest release.
    
    Also, exclude prerelease version (any suffix) in tag explicitly.
    
    Fix #10

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,8 +30,8 @@ list_releases() {
 LATEST_VERSION=$(\
   list_releases | \
   jq -r '.[] | .tag_name' | \
-  sed 's/^v//' | \
-  grep -P '\d+\.\d+\.\d+' | \
+  :"Exclude v prefix and pre-release" \
+  grep -oP '\d+\.\d+\.\d+$' | \
   sort --version-sort --reverse | \
   head -n1
 )
